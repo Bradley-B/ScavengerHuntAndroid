2ce083b5ff627033b5492c4f986522b46c6e7c82
Bradley-B <brad.boxer1@gmail.com>
2018-07-01 15:50:41 -0400

    Android Studio and Gradle Update

--- app/src/main/java/com/bradleyboxer/scavengerhunt/MainActivity.java
+++ app/src/main/java/com/bradleyboxer/scavengerhunt/MainActivity.java
@@ -2,16 +2,13 @@ package com.bradleyboxer.scavengerhunt;
 
 import android.Manifest;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -25,7 +22,6 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-import java.security.Security;
 import java.util.ArrayList;
 import java.util.List;
 
