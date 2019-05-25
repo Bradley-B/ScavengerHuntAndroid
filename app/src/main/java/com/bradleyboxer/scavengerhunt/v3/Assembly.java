3a13ab897dfb2e23004be1efdc18f535bf514b33
Bradley-B <brad.boxer1@gmail.com>
2019-05-25 17:36:50 -0400

    Assembly Fix
    Yes, hard coded clues like in Assembly shouldn't exist and I should make an in-app way to create clues. No, I don't care enough to fix it right now.

--- app/src/main/java/com/bradleyboxer/scavengerhunt/v3/Assembly.java
+++ app/src/main/java/com/bradleyboxer/scavengerhunt/v3/Assembly.java
@@ -9,8 +9,12 @@ public class Assembly {
         ScavengerHunt scavengerHunt = new ScavengerHunt(false);
         GeofenceManager geofenceManager = new GeofenceManager(context);
 
+        final String CLUE_1_NAME = "Beeautiful";
+        final String CLUE_2_NAME = "One Step Forward, Two Steps Back";
+        final String CLUE_3_NAME = "Me @ U";
+
         GeoLocation location1 = new GeoLocation(40.592046f, -74.664458f, 20);
-        Clue clue1 = new CompassClue("Beeautiful",
+        Clue clue1 = new CompassClue(CLUE_1_NAME,
                 "Come one, come all, it's time to begin\n\n" +
                 "Discover the treasures that lie herein\n\n" +
                 "We'll start really simple and do some walk'n\n\n" +
@@ -21,10 +25,11 @@ public class Assembly {
                          "hope you aren't at rest \n\n" +
                          "(you) haven't won the contest (yet)", location1);
         clue1.activate();
+        clue1.addChild(CLUE_2_NAME);
         scavengerHunt.addClue(clue1);
 
         GeoLocation location2 = new GeoLocation(40.590845f, -74.667190f, 250);
-        Clue clue2 = new GeofenceClue("One Step Forward, Two Steps Back",
+        Clue clue2 = new GeofenceClue(CLUE_2_NAME,
                 "We're out and about; we need to backtrack\n\n" +
                  "This was a great warmup but we need a (back)pack\n\n" +
                  "Don't think of this as too much a setback\n\n" +
@@ -34,9 +39,10 @@ public class Assembly {
                          "When you're ready to advance\n\n" +
                          "Give luck a chance\n\n" +
                          "And read this poem I've written already", location2, geofenceManager);
+        clue2.addChild(CLUE_3_NAME);
         scavengerHunt.addClue(clue2);
 
-        Clue clue3 = new TextClue("Me @ U",
+        Clue clue3 = new TextClue(CLUE_3_NAME,
                 "It's pretty much time to get out of here\n\n" +
                 "We just have a gate from what I hear is near\n\n" +
                 "To solve this next clue\n\n" +
@@ -55,29 +61,27 @@ public class Assembly {
     public static ScavengerHunt assembleTestScavengerHunt(Context context) {
         ScavengerHunt scavengerHunt = new ScavengerHunt(false);
 
+        final String CLUE_2_NAME = "Home Test";
+        final String CLUE_4_NAME = "Home Test 2";
+        final String CLUE_5_NAME = "Text Input Test";
+
         GeofenceManager geofenceManager = new GeofenceManager(context);
 
         //home test
         GeoLocation location2 = new GeoLocation(40.590845f, -74.667190f, 1000);
-        Clue clue2 = new GeofenceClue("Home Test", "hintText", "solvedText", location2, geofenceManager);
+        Clue clue2 = new GeofenceClue(CLUE_2_NAME, "hintText", "solvedText", location2, geofenceManager);
         clue2.activate();
-
-//        //home depot test
-//        GeoLocation location3 = new GeoLocation(40.575701f, -74.670116f, 1000);
-//        Clue clue3 = new GeofenceClue("Home Depot", "hintText", "solvedText", location3, geofenceManager);
-//        clue3.activate();
+        clue2.addChild(CLUE_4_NAME);
+        scavengerHunt.addClue(clue2);
 
         //compass test
         GeoLocation location4 = new GeoLocation(40.590845f, -74.667190f, 20);
-        Clue clue4 = new CompassClue("Home Test 2", "hintText", "solvedText", location4);
-        //clue4.activate();
+        Clue clue4 = new CompassClue(CLUE_4_NAME, "hintText", "solvedText", location4);
+        clue4.addChild(CLUE_5_NAME);
+        scavengerHunt.addClue(clue4);
 
         //text input test
-        Clue clue5 = new TextClue("Text Input Test", "hintText", "solvedText", "home");
-
-        scavengerHunt.addClue(clue2);
-//        scavengerHunt.addClue(clue3);
-        scavengerHunt.addClue(clue4);
+        Clue clue5 = new TextClue(CLUE_5_NAME, "hintText", "solvedText", "home");
         scavengerHunt.addClue(clue5);
 
         return scavengerHunt;
