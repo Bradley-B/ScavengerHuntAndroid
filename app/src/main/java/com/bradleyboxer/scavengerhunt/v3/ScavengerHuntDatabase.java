37afc6ee581b259b554c3e527718d80164bd3550
Bradley-B <brad.boxer1@gmail.com>
2019-06-29 01:35:42 -0400

    Firebase Upload/Download
    Created ScavengerHuntDatabase which is meant to store all the local scavenger hunts. Right now it also has the powers of uploading and downloading scavenger hunts to the cloud database- this might change later.

---
+++ app/src/main/java/com/bradleyboxer/scavengerhunt/v3/ScavengerHuntDatabase.java
@@ -0,0 +1,109 @@
+package com.bradleyboxer.scavengerhunt.v3;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class ScavengerHuntDatabase {
+
+    public static final String TAG = "FIRESTORE";
+
+    public static final String DB_KEY_DATA = "scavengerHunt";
+    public static final String DB_KEY_NAME = "name";
+
+    private List<ScavengerHunt> localScavengerHunts;
+    private FirebaseFirestore remoteDb;
+
+    public ScavengerHuntDatabase() {
+        remoteDb = FirebaseFirestore.getInstance();
+        localScavengerHunts = new ArrayList<>();
+    }
+
+    public void addScavengerHunt(ScavengerHunt scavengerHunt) {
+        localScavengerHunts.add(scavengerHunt);
+    }
+
+    public void downloadScavengerHunt(final UUID uuid, final Context context) {
+        remoteDb.collection("scavengerHunts").document(uuid.toString()).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful()) {
+                            Log.d(TAG, "document downloaded with id: " + uuid.toString());
+                            DocumentSnapshot document = task.getResult();
+                            Map<String, Object> dbEntry = document.getData();
+
+                            try {
+                                ScavengerHunt scavengerHunt = ScavengerHunt.deserialize((String) dbEntry.get(DB_KEY_DATA), context);
+                                addScavengerHunt(scavengerHunt);
+                            } catch (Exception e) {
+                                Notifications.displayAlertDialog("Error", "Error decrypting scavenger hunt. Please confirm it was uploaded correctly.", context);
+                            }
+
+                        } else {
+                            Log.w(TAG, "error getting document", task.getException());
+                            Notifications.displayAlertDialog("Error", "Error downloading scavenger hunt. Check your internet connection, then try again.", context);
+                        }
+                    }
+                });
+    }
+
+    public void uploadScavengerHunt(ScavengerHunt scavengerHunt, final Context context) {
+        List<Clue> clueList = scavengerHunt.getClueList();
+        final ScavengerHunt scavengerHuntToUpload = new ScavengerHunt(scavengerHunt);
+        scavengerHuntToUpload.getClueList().clear();
+
+        //to store, mark all clues as inactive except the first
+        for(Clue clue : clueList) {
+            Clue resetClue = clue.deepCopy();
+            resetClue.resetState();
+            scavengerHuntToUpload.addClue(resetClue);
+        }
+
+        if(scavengerHuntToUpload.getClueList().isEmpty()) {
+            Notifications.displayAlertDialog("Error", "You can't upload a scavenger hunt with no clues.", context);
+            return;
+        }
+        scavengerHuntToUpload.getClueList().get(0).activate(); //TODO specify starting clues instead of always activating the first
+
+        Map<String, Object> dbEntry = new HashMap<>();
+        try {
+            String toUpload = ScavengerHunt.serialize(scavengerHuntToUpload);
+            dbEntry.put(DB_KEY_DATA, toUpload);
+            dbEntry.put(DB_KEY_NAME, scavengerHuntToUpload.getName());
+
+            //upload to firestore
+            remoteDb.collection("scavengerHunts")
+                    .document(scavengerHuntToUpload.getUuid().toString()).set(dbEntry)
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "error adding document", e);
+                            Notifications.displayAlertDialog("Error", "Error uploading scavenger hunt. Check your internet connection, then try again.", context);
+                        }
+                    })
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void v) {
+                            Log.d(TAG, "document added with id " + scavengerHuntToUpload.getUuid().toString());
+                        }
+                    });
+        } catch (Exception e) {
+            Notifications.displayAlertDialog("Error", "Error encrypting scavenger hunt. Please try again.", context);
+        }
+    }
+}
